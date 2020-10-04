ee0d49f88575dbb1d56c245274175037939a1068
Carl Dawson <carl@invisible-gorilla.com>
2020-10-04 18:28:12 -0400

    Added router injectable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'loading';
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    this.router.events
+      .subscribe(e => {
+        switch (true) {
+          case e instanceof NavigationStart:
+            console.log('Loading start...');
+            break;
+          case e instanceof NavigationEnd:
+          case e instanceof NavigationError:
+          case e instanceof NavigationCancel:
+            console.log('Loading stop...');
+            break;
+        }
+      });
+  }
 }
